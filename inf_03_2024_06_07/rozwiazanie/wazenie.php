68ca43b40e426b4133f1e329ebe901684454c04b
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2025-06-07 16:40:44 +0200

    refactor(inf_03_2024_06_07): change order of random generating numbers in SQL query

--- inf_03_2024_06_07/rozwiazanie/wazenie.php
+++ inf_03_2024_06_07/rozwiazanie/wazenie.php
@@ -73,7 +73,7 @@ $db = new mysqli("localhost", "root", "", "wazenietirow");
 
         <?php
         $insert_new_weight_query = "INSERT INTO wagi (lokalizacje_id, waga, rejestracja, dzien, czas)
-            VALUES (5, FLOOR(1 + RAND() * 10), 'DW12345', CURRENT_DATE(), CURRENT_TIME())";
+            VALUES (5, FLOOR(RAND() * 10 + 1), 'DW12345', CURRENT_DATE(), CURRENT_TIME())";
         $db->query($insert_new_weight_query);
         ?>
 
