316803cf700b8397545c1eae3f95b94a74552f0d
Tymoteusz Hołubowicz <103210917+holubowicz@users.noreply.github.com>
2025-06-07 21:41:40 +0200

    feat: add inf_03_2025_01_02 solution

---
+++ inf_03_2025_01_02/rozwiazanie/script.js
@@ -0,0 +1,37 @@
+const roomWidthInputElement = document.getElementById("room-width-input");
+const roomLengthInputElement = document.getElementById("room-length-input");
+
+const floorPanelRadio1Element = document.getElementById("floor-panel-radio--1");
+const floorPanelRadio2Element = document.getElementById("floor-panel-radio--2");
+const floorPanelRadio3Element = document.getElementById("floor-panel-radio--3");
+
+const costResultElement = document.getElementById("cost-result");
+
+function handleCalculateButtonOnClick() {
+    const widthValue = roomWidthInputElement.value;
+    const lengthValue = roomLengthInputElement.value;
+
+    if (widthValue === "" || lengthValue === "") {
+        costResultElement.textContent = "Wprowadź poprawne dane.";
+        return;
+    }
+
+    let squareMeterCost = 0;
+    if (floorPanelRadio1Element.checked) {
+        squareMeterCost = 12;
+    } else if (floorPanelRadio2Element.checked) {
+        squareMeterCost = 14;
+    } else if (floorPanelRadio3Element.checked) {
+        squareMeterCost = 18;
+    }
+
+    if (squareMeterCost === 0) {
+        costResultElement.textContent = "Wprowadź poprawne dane.";
+        return;
+    }
+
+    const area = Number(widthValue) * Number(lengthValue);
+    const cost = area * squareMeterCost;
+
+    costResultElement.textContent = `Pole powierzchni pomieszczenia: ${area}, koszt montażu ${cost}`;
+}
